c26e69710728f3711a01389edf0f606ab1229a88
tnngynz <nguyen.tt1703@gmail.com>
2026-04-02 14:46:38 +0700

    setup services

---
+++ services/movie-service/src/test/java/com/princez1/movie_service/MovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.princez1.movie_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
